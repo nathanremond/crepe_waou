d291321320f646ba761209967cf0efe58e94cb22
nathanremond <perso.nathan.remond@gmail.com>
2024-12-08 13:31:32 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 sudo apt install apache2 -y
 sudo apt install php8.3-cli -y
 sudo apt-get install composer -y
+sudo apt install mysql-server -y
+sudo systemctl start mysql.service
+sudo apt install adminer -y
+sudo a2enconf adminer
+sudo systemctl reload apache2

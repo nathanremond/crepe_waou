7c79c2d39cbec608e5b7ce8fa0fdf1970dad01a3
nathanremond <perso.nathan.remond@gmail.com>
2024-12-10 17:27:24 +0100

    Correction install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt install apache2 -y
 sudo apt install php8.3-cli -y
 sudo apt-get install composer -y
+composer install
 sudo apt install mysql-server -y
 sudo systemctl start mysql.service
 sudo apt install adminer -y
